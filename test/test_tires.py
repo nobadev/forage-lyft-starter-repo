a647716f090ce2405a8867548cf2d7a44bef7029
nobadev <bassimxec@gmail.com>
2023-03-12 03:58:53 +0000

    added tire test cases

---
+++ test/test_tires.py
@@ -0,0 +1,44 @@
+import unittest
+from datetime import datetime
+
+from tire.models.CarriganTire import CarriganTire
+from tire.models.OctoprimeTire import OctoprimeTire
+
+class TestCarriganTire(unittest.TestCase):
+  def test_needs_service(self):
+    wear_sensors = [0, 0.2, 1, 0.4]
+
+    tire = CarriganTire(wear_sensors)
+    self.assertTrue(tire.needs_service())
+
+  def test_needs_service_equal(self):
+    wear_sensors = [0, 0.2, 0.9, 0.4]
+
+    tire = CarriganTire(wear_sensors)
+    self.assertTrue(tire.needs_service())
+
+  def test_no_service_equal(self):
+    wear_sensors = [0, 0.2, 0.6, 0.4]
+
+    tire = CarriganTire(wear_sensors)
+    self.assertFalse(tire.needs_service())
+
+class TestOctoprimeTire(unittest.TestCase):
+  def test_needs_service(self):
+    wear_sensors = [1, 1, 1, 1]
+
+    tire = OctoprimeTire(wear_sensors)
+    self.assertTrue(tire.needs_service())
+
+  def test_needs_service_equal(self):
+    wear_sensors = [0.75, 0.75, 0.75, 0.75]
+
+    tire = OctoprimeTire(wear_sensors)
+    self.assertTrue(tire.needs_service())
+
+  def test_no_service_equal(self):
+    wear_sensors = [0, 0.2, 0.6, 0.4]
+
+    tire = OctoprimeTire(wear_sensors)
+    self.assertFalse(tire.needs_service())        
+    
